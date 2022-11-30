1ac99465c80f0b6c2be3ee92959076ad137f9c7d
Victor Ukafor <victorukafor@gmail.com>
2022-11-30 20:03:52 +0100

    save

--- script.js
+++ script.js
@@ -28,3 +28,64 @@ function openAns(e){
 }
 
 
+
+
+
+
+var swiper = new Swiper('.swiper', {
+    slidesPerView: 4,
+    direction: getDirection(),
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    on: {
+      resize: function () {
+        swiper.changeDirection(getDirection());
+      },
+    },
+  });
+
+  function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+    return direction;
+  }
+
+
+  window.onload = function(e){ 
+
+    window.setInterval(function(){
+
+      const swiper = document.querySelector('.swiper-wrapper');
+
+      const currentLast = swiper.lastElementChild.id;
+
+      if(currentLast == 'slider-1'){
+        const element = document.getElementById('slider-2');
+        swiper.appendChild(element);
+      }
+
+      if(currentLast == 'slider-2'){
+        const element = document.getElementById('slider-3');
+        swiper.appendChild(element);
+      }
+
+      if(currentLast == 'slider-3'){
+        const element = document.getElementById('slider-4');
+        swiper.appendChild(element);
+      }
+
+      if(currentLast == 'slider-4'){
+        const element = document.getElementById('slider-1');
+        swiper.appendChild(element);
+      }
+
+      document.querySelector('.swiper-button-next').click();
+
+    }, 5000);
+
+  }
+
+
